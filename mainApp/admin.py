7d671e411a9d4cfa398c57e8104ed59b1f115594
DilshodbekMahammadov <mahammadov1321@gmail.com>
2025-02-09 22:15:31 +0500

    vazifalar

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Yonalish)
-admin.site.register(Fan)
-admin.site.register(Ustoz)
+class UstozAdmin(admin.ModelAdmin):
+    list_display = ['ism', 'daraja']
+    search_fields = ['ism']
+    search_help_text = 'Ustoz ismini kiriting'
+
+class YonalishAdmin(admin.ModelAdmin):
+    list_display = ['nom', 'aktiv']
+    list_filter = ['aktiv']
+    search_fields = ['nom']
+
+class FanAdmin(admin.ModelAdmin):
+    list_filter = ['asosiy', 'yonalish']
+    search_fields = ['nom']
+
+admin.site.register(Yonalish, YonalishAdmin)
+admin.site.register(Fan, FanAdmin)
+admin.site.register(Ustoz, UstozAdmin)
--- mainApp/views.py
+++ mainApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.template.context_processors import request
 
 from .models import *
+from django import forms
+from .forms import *
 
 
 def home_view(request):
@@ -17,16 +19,17 @@ def fanlar_view(request):
 
 
 def fanlar_update_view(request, pk):
+    form = FanForm
     if request.method == 'POST':
-        fan = Fan.objects.filter(pk=pk)
-        fan.update(
-            nom=request.POST.get('nom'),
-            aktiv=request.POST.get('aktiv')
-        )
+        fan_form = FanForm(request.POST)
+        if fan_form.is_valid():
+            fan_form.save()
+
         return redirect('fanlar')
     fan = get_object_or_404(Fan, id=pk)
     context = {
         'fan': fan,
+        'form' : form,
     }
     return render(request, 'fanlar_update.html', context)
 
@@ -53,11 +56,11 @@ def ustozlar_update_view(request, pk):
             jins=request.POST.get('jins'),
             yosh=request.POST.get('yosh'),
             daraja=request.POST.get('daraja'),
-            fan=request.POST.get('fan'),
+            fan_id=request.POST.get('fan_id'),
         )
         return redirect('ustozlar')
     ustoz = get_object_or_404(Ustoz, id=pk)
-    fan = Fan.objects.exclude(id=ustoz.fan.id)
+    fan = Fan.objects.exclude(id=ustoz.fan.id).order_by("nom")
     context = {
         'ustoz' : ustoz,
         'fan' : fan
@@ -82,39 +85,45 @@ def ustozlar_view(request):
 
 
 def yonalish_qoshish_view(request):
+    form = YonalishForm
     if request.method == "POST":
-        Yonalish.objects.create(
-            nom=request.POST.get('nom'),
-            aktiv=request.POST.get('aktiv') == 'on',
-        )
+        yonalish_form = YonalishForm(request.POST)
+        if yonalish_form.is_valid():
+            yonalish_form.save()
         return redirect("/yonalishlar/")
-    return render(request, "yonalish_qoshish.html")
+    context = {
+        'form' : form,
+    }
+    return render(request, "yonalish_qoshish.html", context)
 
 
 def fanlar_qoshish_view(request):
+    form = FanForm
     if request.method == "POST":
+        fan_form = FanForm(request.POST)
+        if fan_form.is_valid():
+            fan_form.save()
         yonalish_id = request.POST.get('yonalish')
 
         if yonalish_id != 'None':
             yonalish = Yonalish.objects.get(id=yonalish_id)
         else:
             yonalish = None
-        Fan.objects.create(
-            nom=request.POST.get('nom'),
-            asosiy=request.POST.get('asosiy') == 'on',
-            yonalish=yonalish,
-
-        )
         return redirect("/fanlar/")
     yonalish = Yonalish.objects.all()
     context = {
         "yonalish": yonalish,
+        'form' : form,
     }
     return render(request, "fanlar_qoshish.html", context)
 
 
 def ustoz_qoshish_view(request):
+    form = UstozForm
     if request.method == "POST":
+        ustoz_form = UstozForm(request.POST)
+        if ustoz_form.is_valid():
+            ustoz_form.save()
         nom = request.POST.get('nom')
         jins = request.POST.get('jins')
         yosh = request.POST.get('yosh')
@@ -125,19 +134,14 @@ def ustoz_qoshish_view(request):
         if fan_id != 'None':
             fan = Fan.objects.get(id=fan_id)
 
-        Ustoz.objects.create(
-            nom=nom,
-            jins=jins,
-            yosh=yosh,
-            daraja=daraja,
-            fan=fan
-        )
+
 
         return redirect("/ustozlar/")
 
     fanlar = Fan.objects.all()
     context = {
         "fanlar": fanlar,
+        'form' : form,
     }
 
     return render(request, "ustozlar_qoshish.html", context)
